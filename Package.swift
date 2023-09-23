6dea7c839bfb5502d7fb67c32d0d7300ec836b62
fumoboy007 <2100868+fumoboy007@users.noreply.github.com>
2023-09-22 23:08:49 -0700

    Fix `pkg-config` search when the package manifest is evaluated via Xcode.

--- Package.swift
+++ Package.swift
@@ -5,24 +5,41 @@ import PackageDescription
 
 enum PkgConfig {
    static func hasLibrary(named libraryName: String) -> Bool {
-      return hasBrewLibrary(named: libraryName) || hasNonBrewLibrary(named: libraryName)
+      #if os(macOS)
+      if hasMacOSBrewLibrary(named: libraryName) {
+         return true
+      }
+      #endif
+
+      return isPkgConfigInPATHAndHasLibrary(named: libraryName)
    }
 
-   private static func hasBrewLibrary(named libraryName: String) -> Bool {
-      let executablePath = "/opt/homebrew/bin/pkg-config"
-      guard FileManager.default.fileExists(atPath: executablePath) else {
-         return false
+   #if os(macOS)
+   private static func hasMacOSBrewLibrary(named libraryName: String) -> Bool {
+      // Hack: We need to hard-code these paths when the manifest is evaluated via Xcode
+      // because Xcode does not properly set up the `PATH` environment variable.
+      let executablePaths = [
+         "/opt/homebrew/bin/pkg-config",
+         "/usr/local/bin/pkg-config"
+      ]
+      for executablePath in executablePaths {
+         guard FileManager.default.fileExists(atPath: executablePath) else {
+            continue
+         }
+
+         let process = try! Process.run(URL(fileURLWithPath: executablePath, isDirectory: false), arguments: [
+            libraryName
+         ])
+         process.waitUntilExit()
+
+         return process.terminationStatus == EXIT_SUCCESS
       }
 
-      let process = try! Process.run(URL(fileURLWithPath: executablePath, isDirectory: false), arguments: [
-         libraryName
-      ])
-      process.waitUntilExit()
-
-      return process.terminationStatus == EXIT_SUCCESS
+      return false
    }
+   #endif
 
-   private static func hasNonBrewLibrary(named libraryName: String) -> Bool {
+   private static func isPkgConfigInPATHAndHasLibrary(named libraryName: String) -> Bool {
       let process = try! Process.run(URL(fileURLWithPath: "/usr/bin/env", isDirectory: false), arguments: [
          "pkg-config", libraryName
       ])
